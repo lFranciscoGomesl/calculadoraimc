fa9d7bf572eae13bdc989b46c3f8676d4a1b6249
FranciscoGomes <franciscofgsn@gmail.com>
2024-06-09 20:32:09 +0100

    TextInput

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,29 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Associar o xml com MainActivity
+
+        val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener {
+            val Peso = edtPeso.text
+            val Altura = edtAltura.text
+            println("Clicou no Butao " + Altura + Peso)
+        }
+
+
+
+
     }
 }
